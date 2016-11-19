662d222723e9b794422eff35743f88a549556310
Jeremy Fu <Jeremy Fu>
2016-11-20 00:37:17 +1100

    hello world

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+//import React from 'react';
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<h1>Testing123</h1>
+		)
+	}
+});
+
+module.exports = App;
